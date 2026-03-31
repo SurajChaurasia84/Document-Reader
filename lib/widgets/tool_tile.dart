6c268096577456a21b1f6d9ffa3efbd394fe348f
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-03-31 13:06:31 +0530

    Updated files

--- lib/widgets/tool_tile.dart
+++ lib/widgets/tool_tile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../models/tool_action.dart';
-import 'glass_card.dart';
 
 class ToolTile extends StatelessWidget {
   const ToolTile({super.key, required this.tool, required this.onTap});
@@ -11,22 +10,55 @@ class ToolTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GlassCard(
-      onTap: onTap,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Icon(IconData(tool.icon, fontFamily: 'MaterialIcons')),
-          const SizedBox(height: 20),
-          Text(tool.title, style: Theme.of(context).textTheme.titleLarge),
-          const SizedBox(height: 8),
-          Text(
-            tool.description,
-            style: Theme.of(
-              context,
-            ).textTheme.bodyMedium?.copyWith(color: Colors.white70),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(16),
+        child: Ink(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[Color(0xFF203D78), Color(0xFF152C58)],
+            ),
+            border: Border.all(color: Colors.white.withValues(alpha: 0.10)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: const Color(0xFF2D66FF).withValues(alpha: 0.20),
+                blurRadius: 18,
+                spreadRadius: 1,
+              ),
+            ],
           ),
-        ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  IconData(tool.icon, fontFamily: 'MaterialIcons'),
+                  color: Colors.white,
+                  size: 24,
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  tool.title,
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                    height: 1.2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
